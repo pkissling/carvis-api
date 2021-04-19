c1eeb2a171057117a59d5424266f39048811d4c4
Patrick Kissling <patrick.kissling@gmx.de>
2021-04-19 21:54:28 +0200

    Inital commit

---
+++ cars_get/src/main.rs
@@ -0,0 +1,23 @@
+use lambda_http::{
+    handler,
+    lambda_runtime::{self, Context},
+    IntoResponse, Request, RequestExt, Response,
+};
+
+type Error = Box<dyn std::error::Error + Send + Sync + 'static>;
+
+#[tokio::main]
+async fn main() -> Result<(), Error> {
+    lambda_runtime::run(handler(func)).await?;
+    Ok(())
+}
+
+async fn func(event: Request, _: Context) -> Result<impl IntoResponse, Error> {
+    Ok(match event.query_string_parameters().get("first_name") {
+        Some(first_name) => format!("Hey, {}!", first_name).into_response(),
+        _ => Response::builder()
+            .status(400)
+            .body("first_name not provided".into())
+            .expect("failed to render response"),
+    })
+}
