5cedcc3377fc9d5284d68cf210718813ecf685d3
Patrick Kissling <patrick.kissling@gmx.de>
2021-10-26 18:50:03 +0200

    Get rid of dummy attributes

--- src/main/kotlin/cloud/carvis/backend/auth/CustomAuth0RoleConverter.kt
+++ src/main/kotlin/cloud/carvis/backend/auth/CustomAuth0RoleConverter.kt
@@ -3,7 +3,7 @@ package cloud.carvis.backend.auth
 import com.nimbusds.jose.shaded.json.JSONArray
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.core.convert.converter.Converter
-import org.springframework.security.oauth2.core.user.OAuth2UserAuthority
+import org.springframework.security.core.GrantedAuthority
 import org.springframework.security.oauth2.jwt.Jwt
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken
 import org.springframework.stereotype.Component
@@ -12,7 +12,8 @@ import java.util.*
 @Component
 class CustomAuth0RoleConverter(@Value("\${auth.custom-role-claim-key}") val claimKey: String): Converter<Jwt, JwtAuthenticationToken> {
 
-    override fun convert(source: Jwt): JwtAuthenticationToken = source.claims[claimKey].let { customRoles ->
+    override fun convert(source: Jwt): JwtAuthenticationToken {
+        val customRoles = source.claims[claimKey]
         if (customRoles == null || customRoles !is JSONArray) {
             return JwtAuthenticationToken(source)
         }
@@ -20,14 +21,12 @@ class CustomAuth0RoleConverter(@Value("\${auth.custom-role-claim-key}") val clai
         val authorities = customRoles
             .map { it.toString() }
             .map { ROLE_PREFIX + it.uppercase(Locale.getDefault()) }
-            .map { OAuth2UserAuthority(it, DUMMY_PERMISSION) }
+            .map { GrantedAuthority { it } }
 
         return JwtAuthenticationToken(source, authorities)
     }
 
     companion object {
-        val DUMMY_PERMISSION = mapOf("dummy_key" to "dummy_value")
         const val ROLE_PREFIX = "ROLE_"
     }
-
 }
