a1405f81a4112385b51aad9d224834e85e0cba88
Patrick Kissling <patrick.kissling@de.bosch.com>
2021-11-01 13:20:55 +0100

    Allow dynamic dynamodb table prefix based on env

--- src/main/kotlin/cloud/carvis/backend/config/AmazonDynamoDBConfig.kt
+++ src/main/kotlin/cloud/carvis/backend/config/AmazonDynamoDBConfig.kt
@@ -1,9 +1,15 @@
 package cloud.carvis.backend.config
 
+import cloud.carvis.backend.dao.repositories.CarRepository
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDB
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapperConfig
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapperConfig.TableNameOverride
 import org.socialsignin.spring.data.dynamodb.config.EnableDynamoDBAuditing
 import org.socialsignin.spring.data.dynamodb.repository.config.EnableDynamoDBRepositories
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.data.domain.AuditorAware
@@ -12,8 +18,8 @@ import java.util.*
 
 
 @Configuration
-@EnableDynamoDBRepositories("cloud.carvis.backend.dao.repositories")
 @EnableDynamoDBAuditing
+@EnableDynamoDBRepositories(basePackageClasses = [CarRepository::class])
 class AmazonDynamoDBConfig {
 
     @Bean
@@ -28,4 +34,18 @@ class AmazonDynamoDBConfig {
             Optional.ofNullable(SecurityContextHolder.getContext().authentication?.name)
         }
     }
+
+    @Bean
+    @ConditionalOnProperty("dynamodb.table-name-prefix")
+    fun tableNameOverride(@Value("\${dynamodb.table-name-prefix}") prefix: String): TableNameOverride =
+        TableNameOverride.withTableNamePrefix("$prefix-")
+
+
+    @Bean
+    @ConditionalOnBean(TableNameOverride::class)
+    fun dynamoDBMapperConfig(tableNameOverride: TableNameOverride): DynamoDBMapperConfig =
+        DynamoDBMapperConfig.builder()
+            .withTableNameOverride(tableNameOverride)
+            .build()
+
 }
