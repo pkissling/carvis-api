a350770361bb3b38918517a3d5db47254e86c115
Patrick Kissling <patrick.kissling@de.bosch.com>
2021-11-02 10:58:05 +0100

    Don't log actuator calls

--- src/main/kotlin/cloud/carvis/backend/config/HttpConfig.kt
+++ src/main/kotlin/cloud/carvis/backend/config/HttpConfig.kt
@@ -3,6 +3,8 @@ package cloud.carvis.backend.config
 import cloud.carvis.backend.filter.TraceIdFilter
 import cloud.carvis.backend.service.LoggingService
 import cloud.carvis.backend.service.LoggingService.Companion.MDC_TRACE_ID_KEY
+import org.springframework.boot.actuate.autoconfigure.endpoint.web.WebEndpointProperties
+import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.web.error.ErrorAttributeOptions
 import org.springframework.boot.web.servlet.error.DefaultErrorAttributes
 import org.springframework.boot.web.servlet.error.ErrorAttributes
@@ -12,11 +14,12 @@ import org.springframework.web.context.request.WebRequest
 
 
 @Configuration
+@EnableConfigurationProperties(WebEndpointProperties::class)
 class HttpConfig {
 
     @Bean
-    fun traceIdFilter(loggingService: LoggingService): TraceIdFilter =
-        TraceIdFilter(loggingService)
+    fun traceIdFilter(loggingService: LoggingService, webEndpointProperties: WebEndpointProperties): TraceIdFilter =
+        TraceIdFilter(loggingService, webEndpointProperties)
 
     @Bean
     fun errorAttributes(loggingService: LoggingService): ErrorAttributes {
--- src/main/kotlin/cloud/carvis/backend/filter/TraceIdFilter.kt
+++ src/main/kotlin/cloud/carvis/backend/filter/TraceIdFilter.kt
@@ -3,6 +3,7 @@ package cloud.carvis.backend.filter
 import cloud.carvis.backend.service.LoggingService
 import io.sentry.SentryTraceHeader.SENTRY_TRACE_HEADER
 import mu.KotlinLogging
+import org.springframework.boot.actuate.autoconfigure.endpoint.web.WebEndpointProperties
 import java.util.*
 import javax.servlet.Filter
 import javax.servlet.FilterChain
@@ -10,7 +11,10 @@ import javax.servlet.ServletRequest
 import javax.servlet.ServletResponse
 import javax.servlet.http.HttpServletRequest
 
-class TraceIdFilter(private val loggingService: LoggingService) : Filter {
+class TraceIdFilter(
+    private val loggingService: LoggingService,
+    private val webEndpointProperties: WebEndpointProperties
+) : Filter {
 
     private val logger = KotlinLogging.logger {}
 
@@ -19,11 +23,14 @@ class TraceIdFilter(private val loggingService: LoggingService) : Filter {
         val httpRequest = request as HttpServletRequest
         val sentryTrace = httpRequest.getHeader(SENTRY_TRACE_HEADER)
 
-        // TODO exclude actuator endpoint
         if (sentryTrace.isNullOrBlank()) {
             val uuid = UUID.randomUUID().toString()
+            if (this.isActuatorCall(request)) {
+                logger.trace { "Incoming request to [${httpRequest.method} ${httpRequest.requestURI}] did not have a sentry-trace. Generated new trace: $uuid" }
+            } else {
+                logger.info { "Incoming request to [${httpRequest.method} ${httpRequest.requestURI}] did not have a sentry-trace. Generated new trace: $uuid" }
+            }
             loggingService.addTraceId(uuid)
-            logger.info { "Incoming request to [${httpRequest.method} ${httpRequest.requestURI}] did not have a sentry-trace. Generated new trace: $uuid" }
         } else {
             logger.info { "Mapping existing sentry-trace from request: $sentryTrace" }
             loggingService.addTraceId(sentryTrace)
@@ -34,5 +41,8 @@ class TraceIdFilter(private val loggingService: LoggingService) : Filter {
     } finally {
         chain.doFilter(request, response)
     }
+
+    private fun isActuatorCall(request: HttpServletRequest) = request.requestURI.startsWith(webEndpointProperties.basePath)
 }
+
 
