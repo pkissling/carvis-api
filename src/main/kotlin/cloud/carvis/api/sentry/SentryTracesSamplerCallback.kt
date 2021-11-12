aa82c6249a0964f84dd22e6ef6291a5ab8e7725f
Patrick Kissling <patrick.kissling@de.bosch.com>
2021-11-12 16:02:23 +0100

    Dont sample /actuator requests

--- src/main/kotlin/cloud/carvis/api/sentry/SentryTracesSamplerCallback.kt
+++ src/main/kotlin/cloud/carvis/api/sentry/SentryTracesSamplerCallback.kt
@@ -3,7 +3,7 @@ package cloud.carvis.api.sentry
 import io.sentry.SamplingContext
 import io.sentry.SentryOptions.TracesSamplerCallback
 import io.sentry.spring.boot.SentryProperties
-import mu.KotlinLogging
+import org.springframework.boot.actuate.autoconfigure.endpoint.web.WebEndpointProperties
 import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.stereotype.Component
 import javax.servlet.http.HttpServletRequest
@@ -11,28 +11,22 @@ import javax.servlet.http.HttpServletRequest
 
 @EnableConfigurationProperties(SentryProperties::class)
 @Component
-class SentryTracesSamplerCallback(private val sentryProperties: SentryProperties) : TracesSamplerCallback {
+class SentryTracesSamplerCallback(
+    private val sentryProperties: SentryProperties,
+    private val webEndpointProperties: WebEndpointProperties
+) : TracesSamplerCallback {
 
-    private val logger = KotlinLogging.logger {}
-
-    override fun sample(context: SamplingContext): Double {
+    override fun sample(context: SamplingContext): Double? {
         val request = context.customSamplingContext!!["request"] as HttpServletRequest?
 
-        return when (request?.requestURI) {
-            "/actuator" -> 0.0
-            else -> fromProperties()
-        }
-
-    }
-
-    private fun fromProperties(): Double {
-        return if (sentryProperties.tracesSampleRate != null) {
-            sentryProperties.tracesSampleRate!!
-        } else {
-            logger.warn { "Could not determine tracesSampleRate. Returning 0" }
-            0.0
+        return when (isActuatorCall(request)) {
+            true -> 0.0
+            else -> sentryProperties.tracesSampleRate
         }
     }
+
+    private fun isActuatorCall(request: HttpServletRequest?) =
+        request?.requestURI?.startsWith(webEndpointProperties.basePath)
 }
 
 
