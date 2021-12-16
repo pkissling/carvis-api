fbb022ee7b9998f93c4b0be90c6128ea7af3df19
Patrick Kissling <patrick.kissling@de.bosch.com>
2021-12-16 15:15:41 +0100

    Add Sleuth

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 val cliVersion: String by project
 
 plugins {
-    id("org.springframework.boot") version "2.5.5"
+    id("org.springframework.boot") version "2.6.1"
     id("io.spring.dependency-management") version "1.0.11.RELEASE"
     kotlin("jvm") version "1.5.31"
     kotlin("plugin.spring") version "1.5.31"
@@ -20,25 +20,26 @@ java.sourceCompatibility = JavaVersion.VERSION_11
 repositories {
     mavenCentral()
 }
-
-
 dependencies {
     annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
     implementation(platform("com.amazonaws:aws-java-sdk-bom:1.12.100"))
     implementation(platform("io.awspring.cloud:spring-cloud-aws-dependencies:2.3.1"))
+    implementation(platform("org.springframework.cloud:spring-cloud-dependencies:2021.0.0"))
 
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    implementation("io.github.microutils:kotlin-logging-jvm:2.0.10")
+
+    implementation("org.springframework.boot:spring-boot-starter")
     implementation("org.springframework.boot:spring-boot-starter-actuator")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-security")
     implementation("org.springframework.boot:spring-boot-starter-cache")
+    implementation("org.springframework:spring-context-support")
     implementation("org.springframework.security:spring-security-oauth2-resource-server")
     implementation("org.springframework.security:spring-security-oauth2-jose")
-    implementation("org.springframework:spring-context-support")
-    implementation("io.github.microutils:kotlin-logging-jvm:2.0.10")
-    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("com.github.derjust:spring-data-dynamodb:5.1.0")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("com.amazonaws:aws-java-sdk-dynamodb")
     implementation("com.amazonaws:aws-java-sdk-s3")
     implementation("io.sentry:sentry-spring-boot-starter:5.3.0")
@@ -48,6 +49,7 @@ dependencies {
     implementation("com.github.ben-manes.caffeine:caffeine")
     implementation("io.awspring.cloud:spring-cloud-starter-aws-messaging")
     implementation("io.awspring.cloud:spring-cloud-starter-aws-ses")
+    implementation("org.springframework.cloud:spring-cloud-starter-sleuth")
 
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5:1.5.20")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
--- src/main/kotlin/cloud/carvis/api/config/HttpConfig.kt
+++ src/main/kotlin/cloud/carvis/api/config/HttpConfig.kt
@@ -1,12 +1,9 @@
 package cloud.carvis.api.config
 
-import cloud.carvis.api.filter.TraceIdFilter
-import cloud.carvis.api.service.LoggingService
-import cloud.carvis.api.service.LoggingService.Companion.MDC_TRACE_ID_KEY
-import io.sentry.IHub
 import org.springframework.boot.web.error.ErrorAttributeOptions
 import org.springframework.boot.web.servlet.error.DefaultErrorAttributes
 import org.springframework.boot.web.servlet.error.ErrorAttributes
+import org.springframework.cloud.sleuth.Tracer
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.context.request.WebRequest
@@ -16,18 +13,17 @@ import org.springframework.web.context.request.WebRequest
 class HttpConfig {
 
     @Bean
-    fun traceIdFilter(loggingService: LoggingService, sentryHub: IHub): TraceIdFilter =
-        TraceIdFilter(loggingService, sentryHub)
-
-    @Bean
-    fun errorAttributes(loggingService: LoggingService): ErrorAttributes {
+    fun errorAttributes(tracer: Tracer): ErrorAttributes {
         return object : DefaultErrorAttributes() {
             override fun getErrorAttributes(
                 webRequest: WebRequest?,
                 options: ErrorAttributeOptions?
             ): MutableMap<String, Any> =
                 super.getErrorAttributes(webRequest, options).apply {
-                    put(MDC_TRACE_ID_KEY, loggingService.getTraceId())
+                    val traceId = tracer.currentTraceContext()?.context()?.traceId()
+                    if (traceId != null) {
+                        put("traceId", traceId)
+                    }
                 }
         }
     }
